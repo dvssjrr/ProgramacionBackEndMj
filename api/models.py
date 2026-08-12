bdcdb851ba7c81d947c64244585ec0665635c0fe
Dvssjr <damibravoag@gmail.com>
2026-08-12 11:27:01 -0400

    uwu

---
+++ api/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class programmer(models.Model):
+ fullname = models.CharField(max_length=100)
+ nickname = models.CharField(max_length=100)
+ language = models.CharField(max_length=100)
+ age = models.PositiveSmallIntegerField()
+ is_active = models.BooleanField(default=True)
