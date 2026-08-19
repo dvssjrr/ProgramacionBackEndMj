d57db73f049bc03723816bc8a315a542d071f364
Dvssjr <damibravoag@gmail.com>
2026-08-18 23:23:02 -0400

    Actualizacion Django

--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+# from django.shortcuts import render <-- esta libreria no la usamos por ahora
+from rest_framework import viewsets
+from .serializer import ProgrammerSerializer
+from .models import programmer
 # Create your views here.
+class ProgrammerViewSet(viewsets.ModelViewSet):
+ # acá creamos una consulta o QUERY a nuestra tabla, trayendo todos los campos como un objeto.
+ queryset = programmer.objects.all()
+ # Agregamos la clase ProgrammerSerializer que ya tiene el modelo serializado para mostrar
+ serializer_class = ProgrammerSerializer
